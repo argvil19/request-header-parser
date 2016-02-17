4e803ae88aacd72ea3da7ff87a794bc73d955bba
Argenis Villasmil <Argenis Villasmil>
2016-02-17 18:31:33 -0430

    fix software display

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ http.createServer(function(req, res) {
     var ua = uaparser(req.headers['user-agent']);
     response.ipaddress = req.headers['x-forwarded-for'];
     response.language = req.headers["accept-language"].substr(0, req.headers["accept-language"].indexOf(","))
-    response.software = ua.browser.name + " " + ua.browser.version + ";" + ua.os.name + " " + ua.os.version;
+    response.software = ua.os.name + " " + ua.os.version + " " + ua.cpu.architecture;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(response));
 }).listen(port);
