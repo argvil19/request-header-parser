33b808c4af576b8110f5ca1b01bb6e8563cb1943
Argenis Villasmil <Argenis Villasmil>
2016-02-17 18:35:15 -0430

    fix os display

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ http.createServer(function(req, res) {
     var ua = uaparser(req.headers['user-agent']);
     response.ipaddress = req.headers['x-forwarded-for'];
     response.language = req.headers["accept-language"].substr(0, req.headers["accept-language"].indexOf(","))
-    response.software = ua.os.name + " " + ua.os.version + " " + ua.cpu.architecture;
+    response.software = ua.os.name + " " + ua.os.version + ";" + ua.cpu.architecture;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(response));
 }).listen(port);
