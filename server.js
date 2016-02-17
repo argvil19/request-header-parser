ac8b7d01d78806bc99414cbae8c56b50ac8cd693
argvil19 <argvil19@gmail.com>
2016-02-17 22:39:39 +0000

    added package.json

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var http = require("http");
 var port = process.env.PORT || 3000;
+var uaparser = require("ua-parser-js");
 
 http.createServer(function(req, res) {
     var response = {};
+    var ua = uaparser(req.headers['user-agent']);
     response.ipaddress = req.headers['x-forwarded-for'];
-    response.language = req.headers["accept-language"];
-    response.software = req.headers['user-agent'];
+    response.language = req.headers["accept-language"].substr(0, req.headers["accept-language"].indexOf(","))
+    response.software = ua.browser.name + " " + ua.browser.version + ";" + ua.os.name + " " + ua.os.version;
     res.setHeader("Content-Type", "application/json");
-    console.log(req.headers);
     res.end(JSON.stringify(response));
 }).listen(port);
